2bce5ddfbdc5d62e576f8884313907188bf648fb
Maurício Szabo <mauricio.szabo@gmail.com>
2023-08-14 22:33:03 -0300

    Initial commit

---
+++ start.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const {app, BrowserWindow} = require('electron')
+
+app.on('ready', () => {
+  let browser = new BrowserWindow({
+    width: 900, height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  browser.loadURL(path.join('file://', __dirname, '/src/index.html'))
+})
